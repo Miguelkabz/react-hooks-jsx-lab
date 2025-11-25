d57ea11801fd8b315d241086fbd2f7b8f9c598d8
Miguelkabz <kabzmiguel@gmail.com>
2025-11-25 11:45:35 +0300

    done

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,14 @@
 import React from "react";
 import { image } from "../data/data";
 
-function About() {
-  return <div>About</div>;
+function About({ image = "https://i.imgur.com/mV8PQxj.gif" }) {
+  return (
+    <div id="about">
+      <h2>About Me</h2>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+      <img src={image} alt="I made this" />
+    </div>
+  );
 }
 
 export default About;
